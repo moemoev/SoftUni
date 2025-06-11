b8f1b063548771796279ef62fa306b4297cf8d0b
Dimo <dimo.dimow@tu-dortmund.de>
2025-06-11 20:42:35 +0200

    added exercise multiplication function

---
+++ Python projects/Python Advanced/functions advanced/lab/multiplication_function.py
@@ -0,0 +1,12 @@
+def multiply(*args):
+    result = 1
+    for el in args:
+        result *= el
+    return result
+
+
+'''
+TASK:
+Write a function called multiply that can receive any quantity of numbers (integers) as different parameters and returns 
+the result of the multiplication of all of them. Submit only your function in the Judge system.
+'''
