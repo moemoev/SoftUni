e3df2c94924b7ed113c983b206f2717cd7aeb15a
Dimo <dimo.dimow@tu-dortmund.de>
2025-06-18 10:03:14 +0200

    added exercise person

---
+++ Python projects/Python Advanced/functions advanced/lab/person_info.py
@@ -0,0 +1,11 @@
+def get_info(name, town, age):
+    sentence = f"This is {name} from {town} and he is {age} years old"
+    return sentence
+
+
+'''
+TASK:
+Write a function called get_info that receives a name, an age, and a town and returns a string in the format: 
+"This is {name} from {town} and he is {age} years old". Use dictionary unpacking when testing your function. Submit only 
+the function in the judge system.
+'''
