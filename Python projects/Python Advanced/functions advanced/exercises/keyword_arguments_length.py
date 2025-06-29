0f21ff90e81fde31efb22c9ab70587dfb1edc286
Dimo <dimo.dimow@tu-dortmund.de>
2025-06-29 09:29:27 +0200

    added exercise keyword arguments length

---
+++ Python projects/Python Advanced/functions advanced/exercises/keyword_arguments_length.py
@@ -0,0 +1,14 @@
+def kwargs_length(**kwargs):
+    return len(kwargs)
+
+
+dictionary = {}
+
+print(kwargs_length(**dictionary))
+
+
+'''
+TASK:
+Create a function called kwargs_length() that can receive some keyword arguments and return their length. Submit only 
+the function in the judge system.
+'''
