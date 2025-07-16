c6e8a2e8537005606af047f8df88baddd6202b30
moemoev <dimo.dimow@tu-dortmund.de>
2025-07-16 20:55:08 +0200

    added exercise projetcs creation

---
+++ JavaScript projects/JavaScript Basics/First Steps in Programming/Lab/07.projects_creation.js
@@ -0,0 +1,6 @@
+function projectsCreation(architectName, countProjects){
+    let totalTime = countProjects * 3;
+    console.log(`The architect ${architectName} will need ${totalTime} hours to complete ${countProjects} project/s.`);
+}
+
+projectsCreation("Gearge",4);
