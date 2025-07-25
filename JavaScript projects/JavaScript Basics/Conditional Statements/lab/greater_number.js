7a6664b9457a94f3231e6760cb5828919c79d5be
moemoev <dimo.dimow@tu-dortmund.de>
2025-07-25 19:17:26 +0200

    greater number

---
+++ JavaScript projects/JavaScript Basics/Conditional Statements/lab/greater_number.js
@@ -0,0 +1,13 @@
+function greaterNumber(numberOne, numberTwo){
+    if (numberOne > numberTwo){
+        console.log(`${numberOne}`);
+    }
+        else{
+            console.log(`${numberTwo}`);   
+        }
+    
+}
+greaterNumber(6, 5);
+
+// In this example the {}-brackets could have been skipped since only one line follows the conditionals 
+// but better explicit than implicit
