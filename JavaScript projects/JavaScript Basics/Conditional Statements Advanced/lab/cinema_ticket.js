f8ee966a55ac478883102ae492ac030a40cdb1a4
Dimo <dimo.dimow@tu-dortmund.de>
2025-08-02 07:27:04 +0200

    added exercise cinema tickets

---
+++ JavaScript projects/JavaScript Basics/Conditional Statements Advanced/lab/cinema_ticket.js
@@ -0,0 +1,28 @@
+function cinemaTicket(day){
+    let price;
+    switch(day){
+        case "Monday":
+            price = 12;
+            break;
+        case "Tuesday":
+            price = 12;
+            break;
+        case "Wednesday":
+            price = 14;
+            break;
+        case "Thursday":
+            price = 14;
+            break;
+        case "Friday":
+            price = 12;
+            break;
+        case "Saturday":
+            price = 16;
+            break;
+        case "Sunday":
+            price = 16;
+            break;
+    }
+    console.log(price);
+}
+cinemaTicket("Monday");
