a3fcc6b1ca67c96794dece258a2b9346d488b78b
Dimo <dimo.dimow@tu-dortmund.de>
2025-08-19 04:11:19 +0200

    added exercise vowels sum

---
+++ JavaScript projects/JavaScript Basics/For Loop/lab/vowels_sum.js
@@ -0,0 +1,28 @@
+function vowelsSum(word){
+  let sum = 0;
+
+  for (i = 0; i < word.length; i++ ){
+    
+    switch (word[i]){
+      case "a":
+        sum += 1;
+        break;
+      case "e":
+        sum += 2;
+        break;
+      case "i":
+        sum += 3;
+        break;
+      case "o":
+        sum += 4;
+        break;
+      case "u":
+        sum += 5;
+        break;
+      default:
+        break;
+    }
+  }
+  console.log(sum);
+}
+vowelsSum("SoftUni");
