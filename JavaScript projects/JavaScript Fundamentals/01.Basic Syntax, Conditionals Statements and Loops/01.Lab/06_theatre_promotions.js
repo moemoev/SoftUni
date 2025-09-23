41987f3cb0b8ff5a522c7bd427622a291100ab03
Dimo <dimo.dimow@tu-dortmund.de>
2025-09-24 00:43:24 +0200

    added exercise theatre promotions

---
+++ JavaScript projects/JavaScript Fundamentals/01.Basic Syntax, Conditionals Statements and Loops/01.Lab/06_theatre_promotions.js
@@ -0,0 +1,47 @@
+function ticketPrice(day, age){
+        if(0 <= age && age <= 18){
+            switch(day){
+                case 'Weekday':
+                    console.log(`12$`);
+                    break;
+                case `Weekend`:
+                    console.log(`15$`);
+                    break;
+                case `Holiday`:
+                    console.log(`5$`);
+                    break;
+                default:
+                    break;
+            }
+        }else if (18 < age && age <= 64){
+            switch(day){
+                case 'Weekday':
+                    console.log(`18$`);
+                    break;
+                case `Weekend`:
+                    console.log(`20$`);
+                    break;
+                case `Holiday`:
+                    console.log(`12$`);
+                    break;
+                default:
+                    break;
+            }
+        }else if (64 < age && age <= 122){
+            switch(day){
+                case 'Weekday':
+                    console.log(`12$`);
+                    break;
+                case `Weekend`:
+                    console.log(`15$`);
+                    break;
+                case `Holiday`:
+                    console.log(`10$`);
+                    break;
+                default:
+                    break;
+            }
+        }else {
+                console.log(`Error!`);
+            }
+}
