50069eb3b27adf26b74e9cfaae968c995498b8f7
Dimo <dimo.dimow@tu-dortmund.de>
2025-10-01 17:10:16 +0200

    added exercise echo type

---
+++ JavaScript projects/JavaScript Fundamentals/02.Data Types and Variables/01.Lab/01_echo_type.js
@@ -0,0 +1,10 @@
+function paramType(parameter){
+    
+    console.log(typeof(parameter));
+
+    if(typeof(parameter) === 'string' || typeof(parameter) === 'number'){
+        console.log(parameter);
+    }else {
+        console.log(`Parameter is not suitable for printing`);
+    }
+}
