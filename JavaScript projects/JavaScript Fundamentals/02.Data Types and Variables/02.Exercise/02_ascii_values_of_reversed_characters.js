017d27c7998ed467d50e9458209921d1b5cd9401
Dimo <dimo.dimow@tu-dortmund.de>
2025-10-07 06:28:18 +0200

    added exercise asscii values of reversed characters

---
+++ JavaScript projects/JavaScript Fundamentals/02.Data Types and Variables/02.Exercise/02_ascii_values_of_reversed_characters.js
@@ -0,0 +1,7 @@
+function reverseValues(firstChar, secondChar, thirdChar){
+    let charReversed = thirdChar + secondChar + firstChar;
+    console.log(charReversed);
+
+    //let valuesOfCharReversed = thirdChar.charCodeAt(0)
+    console.log(`${thirdChar.charCodeAt(0)} ${secondChar.charCodeAt(0)} ${firstChar.charCodeAt(0)}`);
+}
