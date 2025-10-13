ca8e047ed1b317be1f646c4cebec2605f9187045
Dimo <dimo.dimow@tu-dortmund.de>
2025-10-13 06:51:34 +0200

    added exercise sum first and last array elements

---
+++ JavaScript projects/JavaScript Fundamentals/03.Arrays/01.Lab/01_sum_first_and_last_array_elements.js
@@ -0,0 +1,8 @@
+function sumFirstLast(input){
+    let result = input[0] + input[input.length - 1];
+    console.log(result);
+
+}
+sumFirstLast([20, 30, 40]);
+sumFirstLast([10, 17, 22, 33]);
+sumFirstLast([11, 58, 69]);
