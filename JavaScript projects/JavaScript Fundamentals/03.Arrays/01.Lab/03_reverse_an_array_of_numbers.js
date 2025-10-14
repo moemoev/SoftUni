5130c5537e1c704cbd824824ca6bbbe175c6da68
Dimo <dimo.dimow@tu-dortmund.de>
2025-10-14 06:22:27 +0200

    added exercise reverse an array of numbers

---
+++ JavaScript projects/JavaScript Fundamentals/03.Arrays/01.Lab/03_reverse_an_array_of_numbers.js
@@ -0,0 +1,12 @@
+function reverse(index, arr){
+    let arrReverse = [];
+
+    for(let i = index - 1; i >= 0; i--){
+        arrReverse.push(arr[i]);
+    }
+    console.log(arrReverse.join(' '));
+};
+
+reverse(3, [10, 20, 30, 40, 50]);
+reverse(4, [-1, 20, 99, 5]);
+reverse(2, [66, 43, 75, 89, 47]);
