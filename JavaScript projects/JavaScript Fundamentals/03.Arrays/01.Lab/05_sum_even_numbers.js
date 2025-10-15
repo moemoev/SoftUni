682384f68e36f97ddb9cc8af7976f1124c4c6b33
Dimo <dimo.dimow@tu-dortmund.de>
2025-10-15 06:07:51 +0200

    added exercise sum even numbers

---
+++ JavaScript projects/JavaScript Fundamentals/03.Arrays/01.Lab/05_sum_even_numbers.js
@@ -0,0 +1,13 @@
+function sumEven(arr){
+    let sum = 0;
+
+    for(let el of arr){
+        if(el % 2 === 0){
+            sum += Number(el);
+        }
+    }
+    console.log(sum);
+}
+sumEven(['1','2','3','4','5','6']);
+sumEven(['3','5','7','9']);
+sumEven(['2','4','6','8','10']);
