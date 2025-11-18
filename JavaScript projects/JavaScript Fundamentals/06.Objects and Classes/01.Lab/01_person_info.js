f610342a9cf1aebf3d8dddaf18dd37d7c467b830
Dimo <dimo.dimow@tu-dortmund.de>
2025-11-18 18:15:32 +0100

    add exercise person info

---
+++ JavaScript projects/JavaScript Fundamentals/06.Objects and Classes/01.Lab/01_person_info.js
@@ -0,0 +1,17 @@
+function personInfo(firstName, lastName, age){
+    /*
+    let person ={
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
+    */
+    
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+    
+
+    return person;
+}
