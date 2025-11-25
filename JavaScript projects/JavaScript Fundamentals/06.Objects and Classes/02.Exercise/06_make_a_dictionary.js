71dd29d9559528b3be5fe5d418ded9150d2f6d80
Dimo <dimo.dimow@tu-dortmund.de>
2025-11-25 17:35:12 +0100

    add exercise make a dictionary

---
+++ JavaScript projects/JavaScript Fundamentals/06.Objects and Classes/02.Exercise/06_make_a_dictionary.js
@@ -0,0 +1,31 @@
+function dictionary(input){
+
+    let dict = {}
+
+    for (let el of input){
+    let obj = JSON.parse(el);
+    let term = Object.keys(obj)[0]
+    let definition = obj[term];
+
+    dict[term] = definition;
+    }
+    let sortedTerms = Object.keys(dict).sort((a, b) => a.localeCompare(b))
+    for (let term of sortedTerms){
+        console.log(`Term: ${term} => Definition: ${dict[term]}`);
+    }
+
+}
+
+dictionary([
+'{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+'{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+'{"Boiler":"A fuel-burning apparatus or container for heating water."}', 
+'{"Tape":"A narrow strip of material, typically used to hold or fasten something."}', 
+'{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}']);
+
+dictionary([
+'{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+'{"Cake":"An item of soft sweetfood made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."}',
+'{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+'{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+'{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as Term: Art => Definition: The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power. Term: Cake => Definition: An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated. Term: Cup => Definition: A small bowl-shaped container for drinking from, typically having a handle Term: Music => Definition: Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion. Term: Watermelon => Definition: The large fruit of a plant of the gourd painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} ' ]);
