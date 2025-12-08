9657cab10297d330201673e238d8ce4d513c661e
Dimo <dimo.dimow@tu-dortmund.de>
2025-12-08 13:03:52 +0100

    add exercise substring

---
+++ JavaScript projects/JavaScript Fundamentals/08.Text Processing/01.Lab/02_substring.js
@@ -0,0 +1,9 @@
+function solve(input, num1, num2){
+
+    let substring = input.substring(num1, num1 + num2);
+
+    console.log(substring);
+
+}
+solve('ASentence', 1, 8);
+solve('SkipWord', 4, 7);
