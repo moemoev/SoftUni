306c5d5b367aa3420dee74cc27835fca0c759415
moemoev <dimo.dimow@tu-dortmund.de>
2026-02-24 06:13:41 +0100

    add count by initial

---
+++ SQL/03. Built-in Functions/02 Exercises/25_count_by_initial.sql
@@ -0,0 +1,19 @@
+ALTER TABLE
+	users
+ADD COLUMN 
+	initials CHAR(2);
+
+UPDATE
+	users
+SET 
+	initials = LEFT(first_name, 2);
+
+SELECT
+	initials,
+	COUNT(initials) AS user_count
+FROM 
+	users
+GROUP BY 
+	initials
+ORDER BY
+	user_count DESC, initials ASC;
