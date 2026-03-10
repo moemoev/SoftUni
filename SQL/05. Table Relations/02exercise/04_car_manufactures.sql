f4cd6a415a5752654c608e0d598de75e8b6b6180
Dimo <dimo.dimow@tu-dortmund.de>
2026-03-10 06:09:48 +0100

    add exercise car manufactures

---
+++ SQL/05. Table Relations/02exercise/04_car_manufactures.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS manufacturers(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS production_years(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	established_on DATE,
+	manufacturer_id INT,
+	CONSTRAINT fk_production_years_manufactures 
+		FOREIGN KEY(manufacturer_id)
+		REFERENCES manufacturers(id)
+);
+
+CREATE TABLE IF NOT EXISTS models(
+	id INT GENERATED ALWAYS AS IDENTITY (START WITH 1000 INCREMENT BY 1) PRIMARY KEY,
+	model_name VARCHAR(50),
+	manufacturer_id INT,
+	CONSTRAINT fk_models_manufactures 
+		FOREIGN KEY(manufacturer_id)
+		REFERENCES manufacturers(id)
+);
+
+INSERT INTO 
+	manufacturers(name)
+VALUES 
+	('BMW'),
+	('Tesla'),
+	('Lada');
+
+INSERT INTO 
+	production_years(established_on, manufacturer_id)
+VALUES
+	('1916-03-01', 1),
+	('2003-01-01', 2),
+	('1966-05-01', 3);
+
+INSERT INTO 
+	models(model_name, manufacturer_id)
+VALUES
+	('X1', 1),
+	('i6', 1),
+	('Model S', 2),
+	('Model X', 2), 
+	('Model 3', 2),
+	('Nova', 3);
+
