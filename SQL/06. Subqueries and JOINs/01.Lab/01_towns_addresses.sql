00e7a2dd3dc5f658b770fc83257911becbf52553
Dimo <dimo.dimow@tu-dortmund.de>
2026-03-13 08:05:08 +0100

    add exercise towns addresses

---
+++ SQL/06. Subqueries and JOINs/01.Lab/01_towns_addresses.sql
@@ -0,0 +1,15 @@
+SELECT
+	a.town_id,
+	t.name AS town_name,
+	a.address_text
+FROM 
+	towns AS t
+JOIN
+	addresses as a
+ON
+	a.town_id = t.town_id
+WHERE 
+	t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY
+	t.town_id ASC,
+	a.address_id ASC;
