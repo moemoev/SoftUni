e2fe82b912af5931ba283c2823b34db76dc0dea1
Dimo <dimo.dimow@tu-dortmund.de>
2026-03-14 08:38:27 +0100

    add exercise employees projects

---
+++ SQL/06. Subqueries and JOINs/01.Lab/03_employees_projects.sql
@@ -0,0 +1,17 @@
+SELECT
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+	p.project_id,
+	p.name AS project_name
+FROM
+	employees As e
+JOIN
+	employees_projects AS ep
+USING
+	(employee_id)
+JOIN
+	projects AS p
+USING
+	(project_id)
+WHERE 
+	p.project_id = 1
