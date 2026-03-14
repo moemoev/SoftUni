77ab3eb3248f7c04c322c04777dbf01f6caf4e1a
Dimo <dimo.dimow@tu-dortmund.de>
2026-03-14 08:37:53 +0100

    add exercise managers

---
+++ SQL/06. Subqueries and JOINs/01.Lab/02_managers.sql
@@ -0,0 +1,14 @@
+SELECT
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+	d.department_id,
+	d.name AS department_name
+FROM
+	departments AS d
+Join 
+	employees AS e
+ON
+	d.manager_id = e.employee_id
+ORDER BY
+	e.employee_id
+LIMIT 5;
