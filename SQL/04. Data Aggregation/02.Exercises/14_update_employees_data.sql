aeaa4401acf6b66b10d70e77fa380cf48f4faf9a
Dimo <dimo.dimow@tu-dortmund.de>
2026-03-26 09:42:10 +0100

    addjusted exercises

--- SQL/04. Data Aggregation/02.Exercises/14_update_employees_data.sql
+++ SQL/04. Data Aggregation/02.Exercises/14_update_employees_data.sql
@@ -11,3 +11,30 @@ SET
 		WHEN hire_date < '2020-03-04' THEN salary + 1500
 		ELSE salary
 	END;
+
+
+--------------------
+-- Looks like Judge want's explicitly to update all rows, was playing around and added a check
+-- so no duoble updates occur in case of double execution of querry plus filtering non-affected rows regarding hire-date
+--------------------
+UPDATE employees
+SET job_title = CASE
+					WHEN hire_date < DATE '2015-01-16'
+						AND job_title NOT LIKE 'Senior %'
+						THEN CONCAT('Senior ', job_title)
+					WHEN hire_date < DATE '2020-03-04'
+						AND job_title NOT LIKE 'Mid-%'
+						THEN CONCAT('Mid-', job_title)
+					ELSE job_title
+				END,
+	salary = 	CASE
+					WHEN hire_date < DATE '2015-01-16' 
+						AND job_title NOT LIKE 'Senior %'
+						THEN salary + 2500
+					WHEN hire_date < DATE '2020-03-04' 
+						AND job_title NOT LIKE 'Mid-%'
+						THEN salary + 1500
+					ELSE salary
+				END 
+WHERE 
+	hire_date < DATE '2020-03-04'
