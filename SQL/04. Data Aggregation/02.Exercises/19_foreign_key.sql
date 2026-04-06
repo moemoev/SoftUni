b723c008d50133b437d02e6ab5ad2e0a22032a59
Dimo <dimo.dimow@tu-dortmund.de>
2026-04-06 07:32:32 +0200

    refactor: updated exercises

--- SQL/04. Data Aggregation/02.Exercises/19_foreign_key.sql
+++ SQL/04. Data Aggregation/02.Exercises/19_foreign_key.sql
@@ -9,3 +9,24 @@ Create TABLE employees_projects(
 	FOREIGN KEY (project_id)
 	REFERENCES projects(id) 
 );
+
+
+-- more explicit
+
+CREATE TABLE employees_projects(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	employee_id INT ,
+	project_id INT ,
+
+	CONSTRAINT fk_employees_projects_employees
+		FOREIGN KEY(employee_id)
+		REFERENCES employees(id),
+
+	CONSTRAINT fk_employees_projects_projects
+		FOREIGN KEY(project_id)
+		REFERENCES projects(id)
+);
+
+
+------------------
+-- both versions will still have to handle uniqueness along the junction table
